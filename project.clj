a7b67df8ea5cd9992ca480b0aa819d726c0402d8
HARUYAMA Seigo <haruyama@unixuser.org>
2014-06-07 13:18:30 +0900

    Upgrade modules.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://example.com/FIXME"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [ring/ring-devel "1.2.2"]
+                 [ring/ring-jetty-adapter "1.3.0"]
+                 [ring/ring-devel "1.3.0"]
                  [ring-basic-authentication "1.0.5"]
                  [environ "0.5.0"]
                  [com.cemerick/drawbridge "0.0.6"]]
