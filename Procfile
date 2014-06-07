dc2b6cd50a1d43a7d9e809f68ea3b2f9b69f2e1f
HARUYAMA Seigo <haruyama@unixuser.org>
2014-06-07 13:11:18 +0900

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/webhook-bridge-standalone.jar clojure.main -m webhook-bridge.web
---
+++ test/webhook_bridge/web_test.clj
@@ -0,0 +1,6 @@
+(ns webhook-bridge.web-test
+  (:require [clojure.test :refer :all]
+            [webhook-bridge.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
